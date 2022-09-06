b58d9db8a37e72c4916a7e300898dd87f77fbbb6
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-09-06 16:17:53 -0500

    Add javascript into menu btn and nav menu

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,9 +1,22 @@
-const btnMobile = document.getElementById('btn-mobile');
+const hamburgerBtn = document.querySelector('.menu-burger-menu');
+const navMenuMobile = document.querySelector('.mobile-menu-nav ');
+const xBtn = document.querySelector('.close-menu');
+const navLink = document.querySelectorAll('.mobile-link');
 
-function toggleMenu() {
-	const nav = document.getElementById('nav');
-	nav.classList.toggle('active');
-}
+hamburgerBtn.addEventListener('click', () => {
+  navMenuMobile.classList.toggle('active');
+  hamburgerBtn.classList.toggle('active');
+  xBtn.classList.toggle('active');
+});
 
-btnMobile.addEventListener('click', toggleMenu);
+xBtn.addEventListener('click', () => {
+  navMenuMobile.classList.remove('active');
+  hamburgerBtn.classList.remove('active');	
+  xBtn.classList.remove('active');
+});
 
+navLink.forEach((element) => element.addEventListener('click', () => {
+  navMenuMobile.classList.remove('active');
+  hamburgerBtn.classList.remove('active');	
+  xBtn.classList.remove('active');
+}));
